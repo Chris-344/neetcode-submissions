be8db6c94a3216ef5c757c7f2cae5bcdacfa4998
Chris-344 <98201595+Chris-344@users.noreply.github.com>
2026-07-29 14:42:17 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        obj1,obj2={},{}
+
+        for i in s:
+            obj1[i]=1+obj1.get(i,0)
+        for j in t:
+            obj2[j]=1+obj2.get(j,0)
+        return obj1==obj2        
