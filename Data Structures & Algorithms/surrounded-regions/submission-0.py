572b287f3f56da19cba1191d7b408a9103fd8b98
Chris-344 <98201595+Chris-344@users.noreply.github.com>
2026-08-07 19:07:13 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROW,COL=len(board),len(board[0])
+        def capture(r,c):
+            if (r==ROW or c==COL or 
+            r<0 or c<0 or board[r][c]!="O"):
+                return
+            board[r][c]="T"
+            capture(r+1,c)
+            capture(r-1,c)
+            capture(r,c+1)
+            capture(r,c-1)
+        
+        for i in range(ROW):
+            for j in range(COL):
+                if (board[i][j]=="O" and (i in [0,ROW-1] or j in [0,COL-1])):
+                    capture(i,j)
+                
+        for i in range(ROW):
+            for j in range(COL):
+                if board[i][j]=="O":
+                    board[i][j]="X"
+        
+        for i in range(ROW):
+            for j in range(COL):
+                if board[i][j]=="T":
+                    board[i][j]="O"
+        
