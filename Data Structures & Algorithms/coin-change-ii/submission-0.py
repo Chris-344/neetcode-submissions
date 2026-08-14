f59289dce913ef19326383f58fdcf01ef5d40cab
Chris-344 <98201595+Chris-344@users.noreply.github.com>
2026-08-14 18:50:36 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        row=[0] * (amount+1)
+        row[-1] = 1
+        total=0
+        for n in range(len(coins)-1,-1,-1):
+            newRow=[0]*(amount+1)
+            newRow[-1] = 1
+            for i in range(len(row)-2,-1,-1):
+                if i+coins[n]<len(row):
+                    newRow[i]=newRow[i+coins[n]]+row[i]
+                else:
+                    newRow[i]=row[i]
+            row=newRow
+
+        return row[0]
